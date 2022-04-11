06f216f7e1d3ee13095e353090f4af519a84b2e6
Pedro Angel <pavg_92@hotmail.com>
2022-04-10 23:14:26 -0500

    add weekly_mission_1 examples

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,18 @@
+class Pokemon {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHello() {
+        console.log(`Mi pokemon ${this.name} te saluda`);
+    }
+
+    sayMessage(message) {
+        console.log(`Mi pokemon ${this.name} dice: ${message}`);
+    }
+
+
+}
+
+module.exports = Pokemon;
